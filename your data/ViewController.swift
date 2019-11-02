97af01b0d976c1d8fa397ca8358e3c1974ba2215
mostafa ayman ahmed <mostafaayman010030@gmail.com>
2019-11-02 23:27:58 +0200

    calc age done form month

--- your data/ViewController.swift
+++ your data/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var laageinyears: UILabel!
+    @IBOutlet weak var laageinmonth: UILabel!
+    @IBOutlet weak var laageindayes: UILabel!
+    @IBOutlet weak var datepicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func getmyage(_ sender: Any) {
+        let currentdate=Date()
+        let userddb=datepicker.date
+        let calender=Calendar.current
+        let componant=calender.dateComponents( [Calendar.Component.day], from:userddb ,to:currentdate)
+        let years:Int=Int(componant.day! / 365)
+        let month:Int=Int((componant.day!-(years*365))/30)
+        let dayes:Int=Int(componant.day!-((years*365)+(month*30)))
+        laageinyears.text="\(years) year"
+        laageinmonth.text="\(month) month"
+        laageindayes.text="\(dayes) day"
+    }
+    
+    
+    
+    
+    
 }
 
